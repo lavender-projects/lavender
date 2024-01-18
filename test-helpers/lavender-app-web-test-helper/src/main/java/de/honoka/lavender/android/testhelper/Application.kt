83136c85b821dad366b8f629a8c9151ec6b1001e
Kosaka Bun <kosaka-bun@qq.com>
2024-01-18 11:10:28 +0800

    web-test-helper实现ProxyController

--- test-helpers/lavender-app-web-test-helper/src/main/java/de/honoka/lavender/android/testhelper/Application.kt
+++ test-helpers/lavender-app-web-test-helper/src/main/java/de/honoka/lavender/android/testhelper/Application.kt
@@ -3,10 +3,23 @@
 package de.honoka.lavender.android.testhelper
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 class LavenderAndroidTestHelperApplication
+
+@EnableConfigurationProperties(MainProperties::class)
+@Configuration
+class MainConfig
+
+@ConfigurationProperties("lavender.test-helper")
+data class MainProperties(
+
+    var proxyPassUrlPrefix: String? = null
+)
 
 fun main(args: Array<String>) {
     runApplication<LavenderAndroidTestHelperApplication>(*args)
---
+++ test-helpers/lavender-app-web-test-helper/src/main/java/de/honoka/lavender/android/testhelper/controller/ProxyController.kt
@@ -0,0 +1,52 @@
+package de.honoka.lavender.android.testhelper.controller
+
+import cn.hutool.http.HttpUtil
+import cn.hutool.http.Method
+import de.honoka.lavender.android.testhelper.MainProperties
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@CrossOrigin
+@RestController
+class ProxyController(
+    private val mainProperties: MainProperties
+) {
+
+    @GetMapping("/{*path}", produces = [ MediaType.APPLICATION_JSON_VALUE ])
+    fun onRequest(
+        @PathVariable path: String,
+        request: HttpServletRequest,
+        response: HttpServletResponse
+    ): String = onRequest(path, null, request, response)
+
+    @RequestMapping("/{*path}", produces = [ MediaType.APPLICATION_JSON_VALUE ])
+    fun onRequest(
+        @PathVariable path: String,
+        @RequestBody body: String?,
+        request: HttpServletRequest,
+        response: HttpServletResponse
+    ): String {
+        val url = "${mainProperties.proxyPassUrlPrefix}$path"
+        return when(request.method.lowercase()) {
+            "get" -> HttpUtil.createGet(url).run {
+                header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                header(HttpHeaders.ACCEPT_CHARSET, "UTF-8")
+                execute().body()
+            }
+            "post", "put", "patch", "delete" -> {
+                HttpUtil.createRequest(Method.valueOf(request.method.uppercase()), url).run {
+                    setRest(true)
+                    header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                    header(HttpHeaders.ACCEPT_CHARSET, "UTF-8")
+                    body(body)
+                    execute().body()
+                }
+            }
+            "options" -> "{}"
+            else -> throw Exception("Unknown request method: ${request.method}")
+        }
+    }
+}
