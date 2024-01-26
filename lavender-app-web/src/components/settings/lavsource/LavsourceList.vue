41df353487c64fbc14ec8c999a9510ac6ebb029c
Kosaka Bun <kosaka-bun@qq.com>
2024-01-26 15:27:17 +0800

    将LavsourceList提取为独立的组件

---
+++ lavender-app-web/src/components/settings/lavsource/LavsourceList.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="lavsource-list" ref="rootDom">
+    <van-list v-model:loading="componentParams.lavsourceList.loading"
+              :finished="componentParams.lavsourceList.loadFinished"
+              :finished-text="componentParams.lavsourceList.loadFinishedText"
+              @load="loadLavsourceList">
+      <van-cell v-for="(item, index) in lavsourceList" :key="item.id"
+                size="large" is-link @click="emits('itemClick', item, index)">
+        <template #title>
+          <div class="item-title">
+            <!--suppress JSUnresolvedReference -->
+            <img :src="item.imgUrl" alt="" />
+            <div class="item-info">
+              <div class="item-name">{{ item.name }}</div>
+              <div class="item-package-name van-ellipsis">{{ item.packageName }}</div>
+            </div>
+          </div>
+        </template>
+      </van-cell>
+    </van-list>
+  </div>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue'
+
+const props = defineProps({
+  loadDataApi: Function
+})
+
+const componentParams = reactive({
+  lavsourceList: {
+    loading: false,
+    loadFinished: false,
+    loadFinishedText: '',
+    loadFailed: false
+  }
+})
+
+const lavsourceList = ref([]) ?? [
+  {
+    id: 0,
+    type: '',
+    name: '',
+    packageName: '',
+    imgUrl: '',
+    baseUrl: ''
+  }
+]
+
+const rootDom = ref()
+
+const emits = defineEmits([ 'itemClick' ])
+
+function loadLavsourceList() {
+  componentParams.lavsourceList.loadFailed = false
+  props.loadDataApi().then(res => {
+    lavsourceList.value = res.data
+  }).catch(() => {
+    componentParams.lavsourceList.loadFailed = true
+  }).finally(() => {
+    componentParams.lavsourceList.loading = false
+    componentParams.lavsourceList.loadFinished = true
+    updateLavsourceListLoadFinishedText()
+  })
+}
+
+function updateLavsourceListLoadFinishedText() {
+  let text
+  if(componentParams.lavsourceList.loadFailed) {
+    text = '加载失败'
+  } else if(lavsourceList.value.length <= 0) {
+    text = '暂无信源'
+  }
+  componentParams.lavsourceList.loadFinishedText = text
+}
+
+defineExpose({
+  lavsourceList,
+  rootDom,
+  updateLavsourceListLoadFinishedText
+})
+</script>
+
+<style scoped lang="scss">
+.lavsource-list {
+  background-color: var(--van-doc-background);
+
+  .item-title {
+    display: flex;
+    align-items: center;
+    height: 45px;
+    line-height: 20px;
+
+    img {
+      width: 45px;
+      height: 100%;
+      margin-right: 10px;
+    }
+
+    .item-info {
+      position: relative;
+      top: 1.2px;
+
+      .item-package-name {
+        max-width: 247.5px;
+        font-size: 14px;
+        color: var(--van-cell-group-title-color);
+      }
+    }
+  }
+}
+
+::v-deep(.lavsource-list) {
+  .van-cell {
+    align-items: center;
+  }
+
+  .van-list__loading, .van-list__finished-text {
+    line-height: unset;
+  }
+}
+</style>
--- lavender-app-web/src/views/settings/lavsource/LavsourceAddLocalView.vue
+++ lavender-app-web/src/views/settings/lavsource/LavsourceAddLocalView.vue
@@ -1,77 +1,22 @@
 <template>
   <top-layer-settings-view title="添加本地信源">
     <van-cell-group inset title="可添加信源">
-      <div class="local-lavsource-list">
-        <van-list v-model:loading="componentParams.localLavsourceList.loading"
-                  :finished="componentParams.localLavsourceList.loadFinished"
-                  :finished-text="componentParams.localLavsourceList.loadFinishedText"
-                  @load="loadLocalLavsourceList">
-          <van-cell v-for="(item, index) in localLavsourceList" :key="item.id"
-                    size="large" is-link @click="addLocalLavsource(item, index)">
-            <template #title>
-              <div class="item-title">
-                <!--suppress JSUnresolvedReference -->
-                <img :src="item.imgUrl" alt="" />
-                <div class="item-info">
-                  <div class="item-name">{{ item.name }}</div>
-                  <div class="item-package-name van-ellipsis">{{ item.packageName }}</div>
-                </div>
-              </div>
-            </template>
-          </van-cell>
-        </van-list>
-      </div>
+      <lavsource-list ref="localLavsourceListComponent"
+                      :load-data-api="lavsourceJsInterface.getLocalLavsourceListCanBeAdded"
+                      @item-click="addLocalLavsource" />
     </van-cell-group>
   </top-layer-settings-view>
 </template>
 
 <script setup>
 import TopLayerSettingsView from '@/components/common/TopLayerSettingsView.vue'
-import { reactive, ref } from 'vue'
+import { ref } from 'vue'
 import lavsourceJsInterface from '@/androidJsInterfaces/lavsourceJsInterface'
 import { showCustomConfirmDialog, showCustomLoadingToast } from '@/utils/popup'
 import messageUtils from '@/utils/message'
+import LavsourceList from '@/components/settings/lavsource/LavsourceList.vue'
 
-const componentParams = reactive({
-  localLavsourceList: {
-    loading: false,
-    loadFinished: false,
-    loadFinishedText: '',
-    loadFailed: false
-  }
-})
-
-const localLavsourceList = ref([]) ?? [
-  {
-    id: 0,
-    name: '',
-    packageName: '',
-    imgUrl: ''
-  }
-]
-
-function loadLocalLavsourceList() {
-  componentParams.localLavsourceList.loadFailed = false
-  lavsourceJsInterface.getLocalLavsourceListCanBeAdded().then(res => {
-    localLavsourceList.value = res.data
-  }).catch(() => {
-    componentParams.localLavsourceList.loadFailed = true
-  }).finally(() => {
-    componentParams.localLavsourceList.loading = false
-    componentParams.localLavsourceList.loadFinished = true
-    updateLocalLavsourceListLoadFinishedText()
-  })
-}
-
-function updateLocalLavsourceListLoadFinishedText() {
-  let text
-  if(componentParams.localLavsourceList.loadFailed) {
-    text = '加载失败'
-  } else if(localLavsourceList.value.length <= 0) {
-    text = '暂无信源'
-  }
-  componentParams.localLavsourceList.loadFinishedText = text
-}
+const localLavsourceListComponent = ref()
 
 function addLocalLavsource(item, index) {
   showCustomConfirmDialog({
@@ -79,11 +24,11 @@ function addLocalLavsource(item, index) {
     message: `确认添加本地信源“${item.name}”吗？`,
     onConfirm: () => {
       let toast = showCustomLoadingToast({
-        teleport: '.local-lavsource-list'
+        teleport: localLavsourceListComponent.value.rootDom
       })
       lavsourceJsInterface.addLocalLavsource(item).then(() => {
-        localLavsourceList.value.splice(index, 1)
-        updateLocalLavsourceListLoadFinishedText()
+        localLavsourceListComponent.value.lavsourceList.splice(index, 1)
+        localLavsourceListComponent.value.updateLavsourceListLoadFinishedText()
         messageUtils.success('添加成功')
       }).finally(() => {
         toast.close()
@@ -94,43 +39,7 @@ function addLocalLavsource(item, index) {
 </script>
 
 <style scoped lang="scss">
-.local-lavsource-list {
-  background-color: var(--van-doc-background);
-
-  .item-title {
-    display: flex;
-    align-items: center;
-    height: 45px;
-    line-height: 20px;
-
-    img {
-      width: 45px;
-      height: 100%;
-      margin-right: 10px;
-    }
-
-    .item-info {
-      position: relative;
-      top: 1.2px;
-
-      .item-package-name {
-        max-width: 247.5px;
-        font-size: 14px;
-        color: var(--van-cell-group-title-color);
-      }
-    }
-  }
-}
-
-::v-deep(.local-lavsource-list) {
-  .van-cell {
-    align-items: center;
-  }
-
-  .van-list__loading, .van-list__finished-text {
-    line-height: unset;
-  }
-
+::v-deep(.lavsource-list) {
   .van-toast--loading {
     min-width: unset;
     min-height: unset;
