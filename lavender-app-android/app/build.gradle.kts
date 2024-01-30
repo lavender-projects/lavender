0a19a35367972f63024605c28e593f0d9a3f56a3
Kosaka Bun <kosaka-bun@qq.com>
2024-01-30 22:07:38 +0800

    引入OrmLite

--- lavender-app-android/app/build.gradle.kts
+++ lavender-app-android/app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("cn.hutool:hutool-all:5.8.18")
     implementation("com.alibaba.fastjson2:fastjson2:2.0.40.android4")
     implementation("org.nanohttpd:nanohttpd:2.3.1")
+    implementation("com.j256.ormlite:ormlite-android:5.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ lavender-app-android/app/src/main/java/de/honoka/lavender/android/util/DatabaseHelper.kt
@@ -0,0 +1,32 @@
+package de.honoka.lavender.android.util
+
+import android.database.sqlite.SQLiteDatabase
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper
+import com.j256.ormlite.support.ConnectionSource
+import com.j256.ormlite.table.TableUtils
+
+abstract class DatabaseHelper(
+    databaseName: String,
+    private val entityClasses: List<Class<*>>
+) : OrmLiteSqliteOpenHelper(GlobalData.application, databaseName, null, 1) {
+
+    override fun onCreate(database: SQLiteDatabase, connectionSource: ConnectionSource) {
+        entityClasses.forEach {
+            TableUtils.createTable(connectionSource, it)
+        }
+    }
+
+    override fun onUpgrade(
+        database: SQLiteDatabase?,
+        connectionSource: ConnectionSource?,
+        oldVersion: Int,
+        newVersion: Int
+    ) {
+
+    }
+}
+
+class DefaultDatabaseHelper : DatabaseHelper(
+    "main.db",
+    listOf()
+)
